505fcd122f898a114694f6cc076be2aea82f4bcb
Niklas Fiekas <niklas.fiekas@googlemail.com>
2011-12-28 21:58:54 +0100

    Test applying a patch.

--- git-port.py
+++ git-port.py
@@ -3,14 +3,33 @@
 """
 """
 
-from git import Repo
+import sys
+
+from git import Git, Repo
 
 def main():
+    if len(sys.argv) != 2:
+        print "Give one patch as an argument."
+        return
+
     Git.git_binary = 'git'
 
+    repo = Repo('.')
+
+    if repo.bare:
+        print "Repo bare."
+
     if repo.is_dirty():
-        print "Repo is dirty. Stash changes."
+        print "Repo is dirty."
         return
+
+    for file in (repo.untracked_files):
+      if not file.endswith('.patch'):
+        print "Untracked non patch file."
+        return
+
+    repo.git.apply(sys.argv[1])
+
 
 if __name__ == '__main__':
     main()
