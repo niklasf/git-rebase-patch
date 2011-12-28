9cfa6791eb9631f67c1e8a294a511914a2800eec
Niklas Fiekas <niklas.fiekas@googlemail.com>
2011-12-28 21:38:03 +0100

    Detect dirty working copies.

---
+++ git-port.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+"""
+
+from git import Repo
+
+def main():
+    Git.git_binary = 'git'
+
+    if repo.is_dirty():
+        print "Repo is dirty. Stash changes."
+        return
+
+if __name__ == '__main__':
+    main()
