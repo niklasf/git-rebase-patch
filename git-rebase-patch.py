4b3334849d071fba91568667d3749ffff4c93a60
Niklas Fiekas <niklas.fiekas@googlemail.com>
2011-12-31 14:08:46 +0100

    Add lot's of TODOs.

--- git-rebase-patch.py
+++ git-rebase-patch.py
@@ -8,14 +8,18 @@ import sys
 from git import Git, Repo
 
 def main():
+    # TODO: Improve argument parsing.
+    # TODO: Optionally fetch patches via HTTP.
     if len(sys.argv) != 2:
         print "Give one patch as an argument."
         return
 
+    # Initialize a repo object.
     Git.git_binary = 'git'
-
     repo = Repo('.')
 
+    # Doesn't work in bare repositories.
+    # TODO: Support bare repositories.
     if repo.bare:
         print "Repo bare."
 
@@ -23,11 +27,15 @@ def main():
         print "Repo is dirty."
         return
 
-#    for file in (repo.untracked_files):
-#      if not file.endswith('.patch'):
-#        print "Untracked non patch file."
-#        return
+    # TODO: Interactively warn.
+    #for file in (repo.untracked_files):
+    #  if not file.endswith('.patch'):
+    #    print "Untracked non patch file."
+    #    return
 
+    # TODO: Make -p0 optional.
+    # TODO: Try not to alter the working copy.
+    # TODO: Start the rebase.
     looping = True
     while looping:
         try:
